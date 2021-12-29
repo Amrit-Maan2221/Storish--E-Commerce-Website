a9ba123ccbf8ab8439166e8480c887807187607c
Amritpal Singh <AmritMaan2221@gmail.com>
2021-12-29 18:56:13 -0500

    Connected to mongo database use Create Index parser not working while connecting to mongodb using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const dotenv = require("dotenv");
+const connectDatabase = require('./config/database')
 
 // config
 dotenv.config({ path: "./backend/config/config.env" });
+
+//connecting to database
+connectDatabase.connectDatabase();
 
 app.listen(4000, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
