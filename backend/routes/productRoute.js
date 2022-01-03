eb58d9daf50232e778411771614953caed03edb7
Amritpal Singh <AmritMaan2221@gmail.com>
2022-01-02 19:27:24 -0500

    More User Routes and product Routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const dotenv = require("dotenv");
 // config
@@ -9,7 +9,15 @@ dotenv.config({ path: "../config/config.env" });
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/products/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
-router.route("/products/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct).get(getProductDetails);
+router.route("/admin/products/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+    .route("/reviews")
+    .get(getProductReviews)
+    .delete(isAuthenticatedUser, deleteReview);
+
 
 module.exports = router;
