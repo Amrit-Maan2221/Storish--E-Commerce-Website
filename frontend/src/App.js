f24bf83a54f82f87a46e7ba00a7a25752ae57f32
Amritpal Singh <AmritMaan2221@gmail.com>
2022-01-04 10:09:30 -0500

    modified Home Component and added Sylying to it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import Footer from "./components/layout/Footer/Footer";
-import Home from "./components/Home/Home"
+import Home from "./components/Home/Home";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Home />
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,8 +2,101 @@
 
 import React from 'react';
 import "./Home.css";
+import ProductCard from "./ProductCard.js";
+
+const products = [{
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}, {
+    name: "Blue T-shirt",
+    images: [
+        {
+            url: "https://i.ibb.co/DRST11n/1.webp"
+        }
+    ],
+    price: "300",
+    _id: "Amrit",
+    ratings: 2,
+    numOfReviews: 256
+}];
+
 
 function Home() {
+    console.log(products);
     return (
         <>
             <div className="banner">
@@ -16,8 +109,14 @@ function Home() {
                     </button>
                 </a>
             </div>
-
             <h2 className="homeHeading">Featured Products</h2>
+
+            <div className="container" id="container">
+                {products &&
+                    products.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))}
+            </div>
         </>
     )
 }
