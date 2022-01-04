fff2bb9ca00c27f8cc9f66d232b58c14f39f21e6
Amritpal Singh <AmritMaan2221@gmail.com>
2022-01-03 23:06:05 -0500

    made footer and header but header not added in app.js to show in layout because react-icons package has some issue and also done some HOme component work"

---
+++ components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Home.css";
+import { CgMouse } from "react-icons/all";
+
+function Home() {
+    return (
+        <>
+            Home
+            {/* <div className="banner">
+                <p>Welcome to Ecommerce</p>
+                <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+                <a href="#container">
+                    <button>
+                        Scroll <CgMouse />
+                    </button>
+                </a>
+            </div>
+
+            <h2 className="homeHeading">Featured Products</h2> */}
+        </>
+    )
+}
+
+export default Home
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/layout/Footer/Footer";
+import Home from "./components/Home/Home"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
