0a01d9d4e9e679da8f16bad1818c61e54fef12b8
Amritpal Singh <AmritMaan2221@gmail.com>
2022-01-18 00:13:14 -0500

    issue with product detail quantity

--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -1,41 +1,43 @@
 //To do-carosl
+import { useParams } from 'react-router-dom';
 
-import React, { useEffect, useState } from 'react';
-import MetaData from "../layout/MetaData";
-import { useSelector, useDispatch } from "react-redux";
+import React, { Fragment, useEffect, useState } from "react";
+// import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
-import { useAlert } from 'react-alert';
+import { useSelector, useDispatch } from "react-redux";
 import {
     clearErrors,
-    getProductDetails
+    getProductDetails,
+    newReview,
 } from "../../action/productAction";
-import Loader from "../layout/Loader/Loader";
-import { useParams } from 'react-router-dom';
-import ReactStars from "react-rating-stars-component";
 import ReviewCard from "./ReviewCard.js";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+import MetaData from "../layout/MetaData";
+import { addItemsToCart } from "../../action/cartAction";
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Button,
+} from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import { NEW_REVIEW_RESET } from "../../constants/productConstants";
 
-
-
-
-function ProductDetails(props) {
+const ProductDetails = () => {
     const dispatch = useDispatch();
-    const { id } = useParams();
     const alert = useAlert();
+    const { id } = useParams();
 
-    const [quantity, setQuantity] = useState(1);
 
     const { product, loading, error } = useSelector(
         (state) => state.productDetails
     );
 
-    useEffect(() => {
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
-
-        dispatch(getProductDetails(id));
-    }, [dispatch, id, error, alert]);
+    // const { success, error: reviewError } = useSelector(
+    //     (state) => state.newReview
+    // );
 
     const options = {
         size: "large",
@@ -44,30 +46,78 @@ function ProductDetails(props) {
         precision: 0.5,
     };
 
-    const decreaseQuantity = () => {
-
-    };
+    const [quantity, setQuantity] = useState(1);
+    console.log(quantity);
+    const [open, setOpen] = useState(false);
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState("");
 
     const increaseQuantity = () => {
+        if (product.Stock <= quantity) return;
 
+        const qty = quantity + 1;
+        setQuantity(qty);
+    };
+
+    const decreaseQuantity = () => {
+        if (1 >= quantity) return;
+
+        const qty = quantity - 1;
+        setQuantity(qty);
     };
 
     const addToCartHandler = () => {
+        dispatch(addItemsToCart(id, quantity));
+        alert.success("Item Added To Cart");
     };
 
     const submitReviewToggle = () => {
         // open ? setOpen(false) : setOpen(true);
     };
+
+    const reviewSubmitHandler = () => {
+        // const myForm = new FormData();
+
+        // myForm.set("rating", rating);
+        // myForm.set("comment", comment);
+        // myForm.set("productId", id);
+
+        // dispatch(newReview(myForm));
+
+        // setOpen(false);
+    };
+
+    useEffect(() => {
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+
+        // if (reviewError) {
+        //     alert.error(reviewError);
+        //     dispatch(clearErrors());
+        // }
+
+        // if (success) {
+        //     alert.success("Review Submitted Successfully");
+        //     dispatch({ type: NEW_REVIEW_RESET });
+        // }
+        dispatch(getProductDetails(id));
+    }, [dispatch, id, error, alert
+        // ,reviewError, success
+    ]);
+
     return (
-        <>
+        <Fragment>
             {loading ? (
                 <Loader />
             ) : (
-                <>
-                    <MetaData title={`${product.name} -- STORISH`} />
+                <Fragment>
+                    <MetaData title={`${product.name} -- ECOMMERCE`} />
                     <div className="ProductDetails">
                         <div>
-                            {product.images &&
+                            {
+                                product.images &&
                                 product.images.map((item, i) => (
                                     <img
                                         className="CarouselImage"
@@ -75,22 +125,24 @@ function ProductDetails(props) {
                                         src={item.url}
                                         alt={`${i} Slide`}
                                     />
-                                ))}
+                                ))
+                            }
                         </div>
+
                         <div>
                             <div className="detailsBlock-1">
                                 <h2>{product.name}</h2>
                                 <p>Product # {product._id}</p>
                             </div>
                             <div className="detailsBlock-2">
-                                <ReactStars {...options} />
+                                <Rating {...options} />
                                 <span className="detailsBlock-2-span">
                                     {" "}
                                     ({product.numOfReviews} Reviews)
                                 </span>
                             </div>
                             <div className="detailsBlock-3">
-                                <h1>{`$${product.price}`}</h1>
+                                <h1>{`₹${product.price}`}</h1>
                                 <div className="detailsBlock-3-1">
                                     <div className="detailsBlock-3-1-1">
                                         <button onClick={decreaseQuantity}>-</button>
@@ -104,6 +156,7 @@ function ProductDetails(props) {
                                         Add to Cart
                                     </button>
                                 </div>
+
                                 <p>
                                     Status:
                                     <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
@@ -111,6 +164,7 @@ function ProductDetails(props) {
                                     </b>
                                 </p>
                             </div>
+
                             <div className="detailsBlock-4">
                                 Description : <p>{product.description}</p>
                             </div>
@@ -120,7 +174,40 @@ function ProductDetails(props) {
                             </button>
                         </div>
                     </div>
+
                     <h3 className="reviewsHeading">REVIEWS</h3>
+
+                    <Dialog
+                        aria-labelledby="simple-dialog-title"
+                        open={open}
+                        onClose={submitReviewToggle}
+                    >
+                        <DialogTitle>Submit Review</DialogTitle>
+                        <DialogContent className="submitDialog">
+                            <Rating
+                                onChange={(e) => setRating(e.target.value)}
+                                value={rating}
+                                size="large"
+                            />
+
+                            <textarea
+                                className="submitDialogTextArea"
+                                cols="30"
+                                rows="5"
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
+                            ></textarea>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={submitReviewToggle} color="secondary">
+                                Cancel
+                            </Button>
+                            <Button onClick={reviewSubmitHandler} color="primary">
+                                Submit
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+
                     {product.reviews && product.reviews[0] ? (
                         <div className="reviews">
                             {product.reviews &&
@@ -131,10 +218,10 @@ function ProductDetails(props) {
                     ) : (
                         <p className="noReviews">No Reviews Yet</p>
                     )}
-                </>)
-            }
-        </>
-    )
-}
-export default ProductDetails
+                </Fragment>
+            )}
+        </Fragment>
+    );
+};
 
+export default ProductDetails;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -17,6 +17,10 @@ import {
     userDetailsReducer,
     userReducer,
 } from "./reducers/userReducer";
+import { cartReducer } from "./reducers/cartReducer";
+
+
+
 
 const reducer = combineReducers({
     products: productsReducer,
@@ -24,9 +28,21 @@ const reducer = combineReducers({
     user: userReducer,
     profile: profileReducer,
     forgotPassword: forgotPasswordReducer,
+    cart: cartReducer,
 
 });
-let initialState = {};
+
+
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem("cartItems")
+            ? JSON.parse(localStorage.getItem("cartItems"))
+            : [],
+        // shippingInfo: localStorage.getItem("shippingInfo")
+        //     ? JSON.parse(localStorage.getItem("shippingInfo"))
+        //     : {},
+    },
+};
 
 
 const middleware = [thunk];
